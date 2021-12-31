3e7f3734d41f1a972a07733db86770905f506794
themicp <themicp@gmail.com>
2021-12-31 20:08:44 +0200

    ground-controls and message-handler are now separate repos

--- ground-controls/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-
-SERVER_PORT = 3210
-
-app.use(express.static('public'))
-app.listen(SERVER_PORT, () => {
-  console.log(`HTTP Server listening on port ${SERVER_PORT}.`)
-})
