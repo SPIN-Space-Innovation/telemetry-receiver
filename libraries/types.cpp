7c6f6c13b5298ca0991d2e5b9655fc8e6d3f2309
themicp <themicp@gmail.com>
2022-01-01 19:12:05 +0200

    Add types.cpp (clone from flight-computer)

---
+++ libraries/types.cpp
@@ -0,0 +1,54 @@
+#include <stdint.h>
+#include "types.h"
+
+String state_to_str(STATE state) {
+  String names[] = {"INVALID_STATE", "SETUP", "IDLE", "CALIBRATION", "READY", "ARM_BACKUP_DEPLOYER", "RESET_BACKUP_DEPLOYER", "EJECTION_TEST_READY",  "EJECTION_TEST_EJECT", "EJECTION_TEST_COMPLETE", "ASCENDING", "APOGEE_TIMEOUT", "DEPLOYING_CHUTE", "RECOVERING"};
+  if ((int)state >= (int)STATE::Count or (int)state < 0) {
+    return "Unknown";
+  }
+  return names[(int)state];
+}
+
+String event_to_str(EVENT event) {
+  String names[] = {"SETUP_COMPLETE", "INIT_CALIBRATION", "CALIBRATION_COMPLETE", "START_BACKUP_COUNTDOWN", "BACKUP_COUNTDOWN_STARTED", "RESET_BACKUP_COUNTDOWN",  "BACKUP_COUNTDOWN_RESETTED", "SET_EJECTION_TEST", "LAUNCHED", "APOGEE_TIMER_TIMEOUT", "APOGEE_DETECTED", "TRIGGER_FTS", "CHUTE_EJECTED"};
+  if ((int)event >= (int)EVENT::Count or (int)event < 0) {
+    return "Unknown";
+  }
+  return names[(int)event];
+}
+
+String stringifyTelemetryMessage(TelemetryMessage message) {
+  String message_str =
+    String((uint8_t)message.type) + "," +
+    String(message.count) + ",";
+
+  if (message.type == MESSAGE_TYPE::DEBUG) {
+    message_str += message.debug_message;
+  } else if (message.type == MESSAGE_TYPE::TELEMETRY) {
+    uint8_t backup_deployer_state = (message.backup_deployer_status & 0xFF00) >> 8;
+    uint8_t backup_deployer_timer = message.backup_deployer_status & 0x00FF;
+
+    message_str +=
+      String(message.met) + "," +
+      String(message.free_memory_kb) + "," +
+      String(message.battery_voltage_mv) + "," +
+      String(state_to_str(message.state)) + "," +
+      String(backup_deployer_state) + "," +
+      String(backup_deployer_timer);
+
+    if (message.state != STATE::SETUP and message.state != STATE::IDLE and message.state != STATE::CALIBRATION) {
+      message_str += "," +
+        String(message.payload.agl_cm) + "," +
+        String(message.payload.acceleration_x) + "," +
+        String(message.payload.acceleration_y) + "," +
+        String(message.payload.acceleration_z) + "," +
+        String(message.payload.gyroscope_x) + "," +
+        String(message.payload.gyroscope_y) + "," +
+        String(message.payload.gyroscope_z) + "," +
+        String(message.payload.gps_fix) + "," +
+        String(message.payload.gps_satellites);
+    }
+  }
+
+  return message_str;
+}
